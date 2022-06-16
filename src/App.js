1f23f1cd57dcb9b49f756805d56ff046b27546d8
chemin <chemin0428@gmail.com>
2022-06-16 11:16:58 +0900

    스타일 컴포넌트 테스트

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${props=>props.bgColor};
+  width: 100px;
+  height: 100px;
+  text-align: center;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <Box bgColor = 'blue'>
+      <Text>네모</Text>
+    </Box>
+    <Circle bgColor='green'>
+      <Text>동그라미</Text>
+    </Circle>
+    </Container>
   );
 }
 
