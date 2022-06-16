c67de738fbac29170f1d67419f3aaa173e9fc0b1
chemin <chemin0428@gmail.com>
2022-06-16 13:53:47 +0900

    스타일 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const Container = styled.div`
 
 const Box = styled.div`
   background-color: ${props=>props.bgColor};
+  border: ${props=>props.borderColor} solid 5px;
   width: 100px;
   height: 100px;
   text-align: center;
@@ -24,10 +25,10 @@ const Text = styled.span`
 function App() {
   return (
     <Container>
-    <Box bgColor = 'blue'>
+    <Box bgColor = 'purple' borderColor="blue">
       <Text>네모</Text>
     </Box>
-    <Circle bgColor='green'>
+    <Circle bgColor='pink' borderColor="green">
       <Text>동그라미</Text>
     </Circle>
     </Container>
